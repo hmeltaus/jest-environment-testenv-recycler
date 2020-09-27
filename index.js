474413b79460d9307532998d0bd5d06bb5d879b3
Henri Meltaus <henri.meltaus@webscale.fi>
2020-09-27 18:13:47 +0300

    wip

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const NodeEnvironment = require("jest-environment-node")
 const r = require("testenv-recycler")
+const path = require("path")
 
 const isInteger = (str) => /^\+?(0|[1-9]\d*)$/.test(str)
 
@@ -8,13 +9,14 @@ class TestEnvRecyclerEnvironment extends NodeEnvironment {
     super(config, context)
     this.docblockPragmas = context.docblockPragmas
     this.testPath = context.testPath
+    this.name = path.basename(this.testPath)
   }
 
   async setup() {
-    console.log("================= Setup =================")
     await super.setup()
 
     this.recycler = new r.Recycler({
+      name: this.name,
       hostname: this.global.process.env.RECYCLER_HOSTNAME,
       basePath: this.global.process.env.RECYCLER_BASEPATH,
       username: this.global.process.env.RECYCLER_USERNAME,
@@ -23,41 +25,50 @@ class TestEnvRecyclerEnvironment extends NodeEnvironment {
 
     const countStr = this.docblockPragmas["testenv-recycler-count"] || "1"
     if (!isInteger(countStr)) {
-      console.log("@testenv-recycler-count must be a positive integer")
+      console.log(
+        this.name + " - @testenv-recycler-count must be a positive integer",
+      )
       throw new Error(`@testenv-recycler-count must be a positive integer`)
     }
 
     const count = parseInt(countStr, 10)
-    const name = this.testPath
 
     try {
       await this.recycler.login()
     } catch (e) {
-      console.log("Failed to log in", e)
+      console.log(this.name + " - Failed to log in", e)
       throw e
     }
 
-    console.log("logged in")
+    console.log(this.name + " - Logged in")
 
     try {
       this.global.reservation = await this.recycler.createReservation({
-        name,
+        name: this.name,
         count,
       })
     } catch (e) {
-      console.log("Failed to create a reservation", e)
+      console.log(this.name + " - Failed to create the reservation", e)
       throw e
     }
-    console.log("Made reservaation")
-
-    console.log("global:", this.global)
   }
 
   async teardown() {
-    console.log("================= Teardown =================")
-    console.log("global:", this.global)
-    if (!this.global.reservation) {
-      await this.recycler.releaseReservation(this.global.reservation.id)
+    if (this.global.reservation) {
+      try {
+        await this.recycler.releaseReservation(this.global.reservation.id)
+        console.log(
+          this.name + " - Released reservation " + this.global.reservation.id,
+        )
+      } catch (e) {
+        console.log(
+          this.name +
+            " - Failed to release the reservation " +
+            this.global.reservation.id,
+          e,
+        )
+        throw e
+      }
     }
     await super.teardown()
   }
